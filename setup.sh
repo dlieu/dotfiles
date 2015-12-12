9ffde01ee8ffb58bd32a9ad382a7a3ca7ecedcf1
David Lieu <david@david.com>
2015-12-12 12:38:09 -0500

    Added .vimrc and setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Set up soft links to configs
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+# Vim - install Vundle
+if [ ! -d ~/.vim/bundle/Vundle.vim ]
+then
+	echo "working on git"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
