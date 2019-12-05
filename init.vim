988ec5d47bd482473d2356ad848ecf58eb3bf4fa
dl <dl@dl.com>
2019-12-05 15:13:18 -0500

    Add neovim init.vim

---
+++ init.vim
@@ -0,0 +1,36 @@
+" # ===============================================================
+" # File:                 $HOME/.config/nvim/init.vim
+" # Purpose:              Setup file for program neovim
+" # Compiled by:          David Lieu
+" # ===============================================================
+
+
+" Specify a directory for plugins
+" - For Neovim: stdpath('data') . '/plugged'
+" - Avoid using standard Vim directory names like 'plugin'
+call plug#begin('~/.vim/plugged')
+
+Plug 'ctrlpvim/ctrlp.vim'
+Plug 'majutsushi/tagbar'
+
+" Initialize plugin system
+call plug#end()
+
+" Run :PlugInstall to install plugins
+
+
+
+" Various settings from an example vimrc
+set history=500		" keep 50 lines of command line history
+set ruler		" show the cursor position all the time
+set showcmd		" display incomplete commands
+set incsearch		" do incremental searching
+set nu!			" show line numbers
+
+set expandtab		" Old setting from mxn, probably for python to use spaces instead of tabs
+set tabstop=4		" Same as above^^
+set shiftwidth=4	" ^^
+
+
+" Color scheme.
+color desert
--- setup.sh
+++ setup.sh
@@ -3,28 +3,36 @@
 # File:                 $HOME/dotfiles/setup.sh
 # Purpose:              Idempotent Setup file for various dot files (config files)
 # Compiled by:          David Lieu
-# Latest update:        December 18, 2015
 # ===============================================================
 
 ###
 # Backup existing dot files
 ###
 echo "Backup existing dot files (if required)..."
-mkdir -p ~/docs/backup
+mkdir -p ~/archive/dotfiles
+
 # Backup vimrc
-if [ -L $HOME/.vimrc ] && [ "$(readlink $HOME/.vimrc)" = "$HOME/dotfiles/.vimrc" ];
+if [ -L $HOME/.vimrc ] && [ "$(readlink $HOME/.vimrc)" = "$HOME/main/workspace/dotfiles/.vimrc" ];
 then
 	:
 else
-	mv $HOME/.vimrc $HOME/docs/backup/.vimrc_$HOSTNAME_$(date +%Y%m%d)
+	mv $HOME/.vimrc $HOME/archive/dotfiles/.vimrc_$HOSTNAME_$(date +%Y%m%d)
+fi
+
+# Backup neovim init.vim
+if [ -L $HOME/.config/nvim/init.vim ] && [ "$(readlink $HOME/.config/nvim/init.vim)" = "$HOME/main/workspace/dotfiles/init.vim" ];
+then
+	:
+else
+	mv $HOME/.config/nvim/init.vim $HOME/archive/dotfiles/nvim_init_vim_$HOSTNAME_$(date +%Y%m%d)
 fi
 
 # Backup screenrc
-if [ -L $HOME/.screenrc ] && [ "$(readlink $HOME/.screenrc)" = "$HOME/dotfiles/.screenrc" ];
+if [ -L $HOME/.screenrc ] && [ "$(readlink $HOME/.screenrc)" = "$HOME/main/workspace/dotfiles/.screenrc" ];
 then
 	:
 else
-	mv $HOME/.screenrc $HOME/docs/backup/.screenrc_$HOSTNAME_$(date +%Y%m%d)
+	mv $HOME/.screenrc $HOME/archive/dotfiles/.screenrc_$HOSTNAME_$(date +%Y%m%d)
 fi
 
 # Alert .vim has more than one folder
@@ -34,19 +42,26 @@ fi
 # Set up soft links to configs
 ###
 echo "Setup sym links to dotfiles (if required)..."
-if [ -L $HOME/.vimrc ] && [ "$(readlink $HOME/.vimrc)" = "$HOME/dotfiles/.vimrc" ];
+if [ -L $HOME/.vimrc ] && [ "$(readlink $HOME/.vimrc)" = "$HOME/main/workspace/dotfiles/.vimrc" ];
 then
 	echo ".vimrc sym link exists already"
 else
-	ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+	ln -s $HOME/main/workspace/dotfiles/.vimrc $HOME/.vimrc
 fi
 
 
-if [ -L $HOME/.screenrc ] && [ "$(readlink $HOME/.screenrc)" = "$HOME/dotfiles/.screenrc" ];
+if [ -L $HOME/.config/nvim/init.vim ] && [ "$(readlink $HOME/.config/nvim/init.vim)" = "$HOME/main/workspace/dotfiles/init.vim" ];
+then
+	echo "neovim init.vim sym link exists already"
+else
+	ln -s $HOME/main/workspace/dotfiles/init.vim $HOME/.config/nvim/init.vim
+fi
+
+if [ -L $HOME/.screenrc ] && [ "$(readlink $HOME/.screenrc)" = "$HOME/main/workspace/dotfiles/.screenrc" ];
 then
 	echo ".screenrc sym link exists already"
 else
-	ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
+	ln -s $HOME/main/workspace/dotfiles/.screenrc $HOME/.screenrc
 fi
 
 ###
